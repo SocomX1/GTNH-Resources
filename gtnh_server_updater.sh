5ece4cc540659a1ba50ad1ed8b56309e2125ea97
Alexander Zucker <SocomX01@gmail.com>
2025-12-20 13:42:42 -0800

    Add .tar.gz extension to backup archive.

--- gtnh_server_updater.sh
+++ gtnh_server_updater.sh
@@ -5,4 +5,4 @@ SERVER_PATH="/home/opc/gtnh"
 echo "Creating archive backup of $SERVER_PATH..."
 cd "$SERVER_PATH" || exit
 ARCHIVE_DATE=$(date +%Y-%m-%d-%H%M%S)
-tar --exclude='backups' --exclude='dynmap' -czvf "$SERVER_PATH/../gtnh_backup_$ARCHIVE_DATE" "$SERVER_PATH"
+tar --exclude='backups' --exclude='dynmap' -czvf "$SERVER_PATH/../gtnh_backup_$ARCHIVE_DATE.tar.gz" "$SERVER_PATH"
