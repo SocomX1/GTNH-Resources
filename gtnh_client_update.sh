54d6f64a9963c02ed2307432b9a0d712fd0e614d
Alexander Zucker <SocomX01@gmail.com>
2025-12-20 01:13:29 -0800

    Comments, fixed zip archive not being deleted.

--- gtnh_client_update.sh
+++ gtnh_client_update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Path of the Prism instance you want to clone
 SOURCE_INSTANCE='/mnt/c/Users/Socom/AppData/Roaming/PrismLauncher/instances/GT_New_Horizons_2.8.0_Java_17-25-migrated'
+
+# Path of the updated Prism instance this script will create
 TARGET_INSTANCE='/mnt/c/Users/Socom/AppData/Roaming/PrismLauncher/instances/GT_New_Horizons_2.8.3_Java_17-25-migrated'
+
+# URL from which to download the new client
 CLIENT_DOWNLOAD='https://downloads.gtnewhorizons.com/Multi_mc_downloads/GT_New_Horizons_2.8.3_Java_17-25.zip'
 
+# Don't touch
 PACK_ARCHIVE="$(basename "$CLIENT_DOWNLOAD")"
 PACK_NAME="${PACK_ARCHIVE%_Java_*}"
 PACK_NAME="${PACK_NAME//_/ }"
@@ -31,6 +37,7 @@ cd .minecraft || exit
 cp -r config serverutilities mods $TARGET_INSTANCE/.minecraft
 
 echo 'Cleaning up temp files...'
-rm -r "$TARGET_INSTANCE/../$PACK_NAME"
+cd $TARGET_INSTANCE/.. || exit
+rm -r "$PACK_NAME" "$PACK_ARCHIVE"
 
 echo 'Done!'
