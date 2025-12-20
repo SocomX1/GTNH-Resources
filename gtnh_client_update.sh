7153bfdbdd6872e710b1e9c8bf0763035d6df733
Alexander Zucker <SocomX01@gmail.com>
2025-12-20 01:38:30 -0800

    Fixed Prism instance name not updating.

--- gtnh_client_update.sh
+++ gtnh_client_update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# MAKE SURE YOUR PRISM LAUNCHER IS CLOSED WHILE RUNNING THIS SCRIPT
+
 # Path of the Prism instance you want to clone
 SOURCE_INSTANCE='/mnt/c/Users/Socom/AppData/Roaming/PrismLauncher/instances/GT_New_Horizons_2.8.0_Java_17-25-migrated'
 
@@ -15,29 +17,31 @@ PACK_NAME="${PACK_ARCHIVE%_Java_*}"
 PACK_NAME="${PACK_NAME//_/ }"
 
 echo 'Copying client instance...'
-cp -r $SOURCE_INSTANCE $TARGET_INSTANCE
+cp -r "$SOURCE_INSTANCE" "$TARGET_INSTANCE"
+cd "$TARGET_INSTANCE" || exit
+sed -i "s/^name=.*/name=$PACK_NAME/" instance.cfg
 
 echo 'Deleting old data from copied instance...'
-cd $TARGET_INSTANCE || exit
+cd "$TARGET_INSTANCE" || exit
 rm -r libraries patches mmc-pack.json
 cd .minecraft || exit
 rm -r config serverutilities mods scripts resources
 
 echo 'Downloading new client files...'
-cd $TARGET_INSTANCE/.. || exit
-wget $CLIENT_DOWNLOAD
+cd "$TARGET_INSTANCE/.." || exit
+wget "$CLIENT_DOWNLOAD"
 
 echo 'Extracting new client files...'
 unzip -o "$PACK_ARCHIVE"
 
 echo 'Copying new client files to instance...'
 cd "$PACK_NAME" || exit
-cp -r libraries patches mmc-pack.json $TARGET_INSTANCE
+cp -r libraries patches mmc-pack.json "$TARGET_INSTANCE"
 cd .minecraft || exit
-cp -r config serverutilities mods $TARGET_INSTANCE/.minecraft
+cp -r config serverutilities mods "$TARGET_INSTANCE/.minecraft"
 
 echo 'Cleaning up temp files...'
-cd $TARGET_INSTANCE/.. || exit
+cd "$TARGET_INSTANCE/.." || exit
 rm -r "$PACK_NAME" "$PACK_ARCHIVE"
 
 echo 'Done!'
