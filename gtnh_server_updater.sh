1c848041f6222d56bbf474de5bdaf39bde08310d
Alex Zucker <SocomX01@gmail.com>
2025-12-23 14:46:33 -0800

    Finished logic for server updater script, ready for first round of testing.

--- gtnh_server_updater.sh
+++ gtnh_server_updater.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
 
+# Path of the currently installed GTNH server
 SERVER_PATH="/home/opc/gtnh"
+
+# URL from which to download the new GTNH server archive
+SERVER_DOWNLOAD_URL="https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.8.4_Server_Java_17-25.zip"
+
+# Path where the backup of the currently installed GTNH server will be created
+BACKUP_ARCHIVE="$SERVER_PATH/../gtnh_backup_$(date +%Y-%m-%d-%H%M%S).tar.gz"
+
+# Don't touch
+PACK_ARCHIVE="$(basename "$SERVER_DOWNLOAD_URL")"
+PACK_NAME="${PACK_ARCHIVE%_Java_*}"
+PACK_NAME="${PACK_NAME//_/ }"
+
 TMPDIR="$(mktemp -d)"
 
 cleanup() {
-    echo "Deleting temp files..."
-    rm -rf "$TMPDIR"
-    echo "Done!"
+    local EXIT_CODE=$?
+
+    if (( EXIT_CODE != 0 )); then
+        echo "Error code $EXIT_CODE encountered, cleaning up temp files..."
+        rm -rf "$TMPDIR" || true
+        echo "Temp files removed, but the script exited prematurely."
+    else
+        echo "Cleaning up temp files..."
+        rm -rf "$TMPDIR" || true
+        echo "Done! $PACK_NAME is ready to play."
+    fi
+
+    echo "Remember to download the backup archive from $BACKUP_ARCHIVE!"
 }
 trap cleanup EXIT
 
 echo "Creating archive backup of $SERVER_PATH..."
-cd "$SERVER_PATH" || exit
-ARCHIVE_DATE=$(date +%Y-%m-%d-%H%M%S)
-tar --exclude='backups' --exclude='dynmap' -czvf "$SERVER_PATH/../gtnh_backup_$ARCHIVE_DATE.tar.gz" "$SERVER_PATH"
+cd "$SERVER_PATH" || exit 1
+tar --exclude='backups' --exclude='dynmap' -czf "$BACKUP_ARCHIVE" "$SERVER_PATH" || exit 1
 
 echo "Backing up $SERVER_PATH/config/JourneyMapServer directory..."
-cp -r "$SERVER_PATH/config/JourneyMapServer" "$TMPDIR"
+cp -r "$SERVER_PATH/config/JourneyMapServer" "$TMPDIR" || exit 1
+
+echo "Downloading new server archive..."
+cd "$TMPDIR" || exit 1
+wget "$SERVER_DOWNLOAD_URL" || exit 1
+
+echo "Extracting new server files..."
+unzip -qo "$PACK_ARCHIVE" || {
+    echo "Error: failed to unzip $PACK_ARCHIVE. Is unzip installed?"
+    exit 1
+}
 
 echo "Deleting old server files..."
-cd "$SERVER_PATH" || exit
+cd "$SERVER_PATH" || exit 1
+rm -rf config libraries mods resources scripts 'lwjgl3ify-forgePatches.jar' 'java9args.txt' # resources and scripts should only exist in older server versions, no need to copy them over
+
+echo "Copying new server files to $SERVER_PATH..."
+cd "$TMPDIR" || exit 1
+cp -r config libraries mods 'lwjgl3ify-forgePatches.jar' 'java9args.txt' "$SERVER_PATH"
+
+echo "Copying saved JourneyMapServer config files to $SERVER_PATH..."
+cd "$TMPDIR" || exit 1
+cp -r "JourneyMapServer" "$SERVER_PATH/config"
