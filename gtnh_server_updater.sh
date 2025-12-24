076836301d6fecad236eb723222c46dddc4acc0c
Alex Zucker <SocomX01@gmail.com>
2025-12-24 12:58:13 -0800

    Added logic for backing up Dynmap plugin.

--- gtnh_server_updater.sh
+++ gtnh_server_updater.sh
@@ -13,6 +13,7 @@ BACKUP_ARCHIVE="$SERVER_PATH/../gtnh_backup_$(date +%Y-%m-%d-%H%M%S).tar.gz"
 PACK_ARCHIVE="$(basename "$SERVER_DOWNLOAD_URL")"
 PACK_NAME="${PACK_ARCHIVE%_Java_*}"
 PACK_NAME="${PACK_NAME//_/ }"
+MAP_PLUGIN_EXISTS=false
 
 TMPDIR="$(mktemp -d)"
 
@@ -35,10 +36,24 @@ trap cleanup EXIT
 
 echo "Creating archive backup of $SERVER_PATH..."
 cd "$SERVER_PATH" || exit 1
-tar --exclude='backups' --exclude='dynmap' -czf "$BACKUP_ARCHIVE" "$SERVER_PATH" || exit 1
+tar --exclude='backups' --exclude='dynmap' -czf "$BACKUP_ARCHIVE" -C "$(dirname "$SERVER_PATH")" "$(basename "$SERVER_PATH")" || exit 1
 
 echo "Backing up $SERVER_PATH/config/JourneyMapServer directory..."
 cp -r "$SERVER_PATH/config/JourneyMapServer" "$TMPDIR" || exit 1
+
+echo "Backing up Dynmap plugin, if it exists..."
+cd "$SERVER_PATH/mods" || exit 1
+shopt -s nullglob # ensure file search expands to nothing if no matches
+MAP_FILES=( *gtnh-web-map* )
+if (( ${#MAP_FILES[@]} > 0 )); then
+    MAP_PLUGIN_EXISTS=true
+    MAP_PLUGIN=${MAP_FILES[0]}
+    echo "Dynmap plugin found: $MAP_PLUGIN"
+    cp "$MAP_PLUGIN" "$TMPDIR"
+else
+    echo "No Dynmap plugin found, proceeding..."
+fi
+shopt -u nullglob
 
 echo "Downloading new server archive..."
 cd "$TMPDIR" || exit 1
@@ -58,6 +73,12 @@ echo "Copying new server files to $SERVER_PATH..."
 cd "$TMPDIR" || exit 1
 cp -r config libraries mods 'lwjgl3ify-forgePatches.jar' 'java9args.txt' "$SERVER_PATH"
 
-echo "Copying saved JourneyMapServer config files to $SERVER_PATH..."
+echo "Copying saved JourneyMapServer config files to $SERVER_PATH/config..."
 cd "$TMPDIR" || exit 1
 cp -r "JourneyMapServer" "$SERVER_PATH/config"
+
+if [[ $MAP_PLUGIN_EXISTS == true ]]; then
+    echo "Copying saved $MAP_PLUGIN plugin to $SERVER_PATH/mods..."
+    cd "$TMPDIR" || exit 1
+    cp "$MAP_PLUGIN" "$SERVER_PATH/mods"
+fi
